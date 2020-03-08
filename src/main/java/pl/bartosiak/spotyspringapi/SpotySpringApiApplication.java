d5ce7ec448c2627ace807a3150a03774ad9a3ab0
zumek1 <2Tupac148>
2020-03-08 22:57:02 +0100

    Initial commit

---
+++ src/main/java/pl/bartosiak/spotyspringapi/SpotySpringApiApplication.java
@@ -0,0 +1,13 @@
+package pl.bartosiak.spotyspringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotySpringApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotySpringApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/bartosiak/spotyspringapi/SpotySpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bartosiak.spotyspringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotySpringApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
